d3bca8599e6bb4d6821b4bcf854f887a062c07b6
Arnon Keereena <a.keereena@gmail.com>
2020-03-21 22:46:43 +0700

    Generate SPM files

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,8 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "4.3.1"),
-      .package(url: "https://github.com/myste1tainn/SwiftExpansion.git", from: "0.1.0"),
+      .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
+      .package(url: "https://github.com/myste1tainn/SwiftExpansion.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
